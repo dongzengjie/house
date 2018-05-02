012190cf483895ce2d0a511beff8c39ea4cdd833
dongzengjie <2857877087@qq.com>
2018-05-02 17:49:00 +0800

    afsfwew

--- house/src/test/java/com/dzj/house/service/UserserviceTest.java
+++ house/src/test/java/com/dzj/house/service/UserserviceTest.java
@@ -1,5 +1,8 @@
 package com.dzj.house.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,10 @@ public class UserserviceTest {
 		String aa="dnhkasakshkd";
 		String bb="casawa";
 		System.out.println(aa.charAt(0)==bb.charAt(0));
+		Map<String, String> map =new HashMap<>();
+		map.put("a", "a");
+		map.put("a", "b");
+		
 	}
 	
 }
